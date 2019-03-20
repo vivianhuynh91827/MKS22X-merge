2aff94cfb61307fc74e864bcee92b0cd04bf541b
vivianhuynh91827 <vhuynh00@stuy.edu>
2019-03-20 07:18:09 -0400

    un-optimized mergesort completed

--- Merge.java
+++ Merge.java
@@ -22,14 +22,35 @@ public class Merge {
     }
     // System.out.println(Arrays.toString(left));
     // System.out.println(Arrays.toString(right));
+    mergeSortH(left, 0, left.length-1);
+    mergeSortH(right, 0, right.length-1);
     int l = 0; //current index of left side
     int r = 0; //current index of right side
     for (int i = 0; i < data.length; i++) {
-      
+      if (l < left.length && r < right.length) { //Compare both values, add the smaller one
+        if (left[l]<=right[r]) {
+          data[i] = left[l];
+          l++;
+        }
+        else if (right[r]<left[l]) {
+          data[i] = right[r];
+          r++;
+        }
+      }
+      else if (l >= left.length && r < right.length) {
+        data[i] = right[r];
+        r++;
+      }
+      else if (l < left.length && r >= right.length) {
+        data[i] = left[l];
+        l++;
+      }
     }
   }
   public static void main(String[] args) {
-    int[] test = {1,2,3,4,5,6,7,8,9,10};
+    int[] test = {10,9,8,7,6,5,4,3,2,1};
+    System.out.println(Arrays.toString(test));
     mergeSortH(test,0,9);
+    System.out.println(Arrays.toString(test));
   }
 }
